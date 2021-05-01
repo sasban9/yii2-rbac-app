95b978cc279a56ebd9910b7f5f66c7aebbe953dc
Niranjan Nimbalkar <niranjan.n@newfuture.foundation>
2021-05-01 14:01:41 +0530

    Changed navigation menu as per extension

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -9,12 +9,15 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
+use webvimark\modules\UserManagement\components\GhostNav;
+use webvimark\modules\UserManagement\UserManagementModule;
 
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,59 +26,95 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body>
-<?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
+<body>
+    <?php $this->beginBody() ?>
+
+    <div class="wrap">
+        <?php
+    // NavBar::begin([
+    //     'brandLabel' => Yii::$app->name,
+    //     'brandUrl' => Yii::$app->homeUrl,
+    //     'options' => [
+    //         'class' => 'navbar-inverse navbar-fixed-top',
+    //     ],
+    // ]);
+    // echo Nav::widget([
+    //     'options' => ['class' => 'navbar-nav navbar-right'],
+    //     'items' => [
+    //         ['label' => 'Home', 'url' => ['/site/index']],
+    //         ['label' => 'About', 'url' => ['/site/about']],
+    //         ['label' => 'Contact', 'url' => ['/site/contact']],
+    //         Yii::$app->user->isGuest ? (
+    //             ['label' => 'Login', 'url' => ['/site/login']]
+    //         ) : (
+    //             '<li>'
+    //             . Html::beginForm(['/site/logout'], 'post')
+    //             . Html::submitButton(
+    //                 'Logout (' . Yii::$app->user->identity->username . ')',
+    //                 ['class' => 'btn btn-link logout']
+    //             )
+    //             . Html::endForm()
+    //             . '</li>'
+    //         )
+    //     ],
+    // ]);
+    // NavBar::end();
+
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
+        //'brandLabel' => 'My Company',
+        //'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    echo Nav::widget([
+
+    echo GhostNav::widget([
+        'encodeLabels' => false,
+        'activateParents' => true,
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            [
+                'label' => 'Backend routes',
+                'items' => UserManagementModule::menuItems(),
+            ],
+            [
+                'label' => 'Frontend routes',
+                'items' => [
+                    ['label' => 'Login', 'url' => ['/user-management/auth/login']],
+                    ['label' => 'Logout', 'url' => ['/user-management/auth/logout']],
+                    ['label' => 'Registration', 'url' => ['/user-management/auth/registration']],
+                    ['label' => 'Change own password', 'url' => ['/user-management/auth/change-own-password']],
+                    ['label' => 'Password recovery', 'url' => ['/user-management/auth/password-recovery']],
+                    ['label' => 'E-mail confirmation', 'url' => ['/user-management/auth/confirm-email']],
+                ],
+            ],
         ],
+
     ]);
+
     NavBar::end();
     ?>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
+        <div class="container">
+            <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
     </div>
-</div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+    <footer class="footer">
+        <div class="container">
+            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+            <p class="pull-right"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
 
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
