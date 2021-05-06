5ee8c0231417efe967dd881784fe0a4e3d2e63bd
Niranjan Nimbalkar <niranjan.n@newfuture.foundation>
2021-05-06 13:50:37 +0530

    Events db dump

---
+++ dumps/Events_202105061349.sql
@@ -0,0 +1,8 @@
+INSERT INTO yii2basic.Events (user_id,category_id,status_id,asset_type,venue_address_id,tkt_details_out_type_id,name,url,description,terms,refund_policy,privacy_policy,short_url,group_id) VALUES
+	 (6,1,1,'1',1,1,'Test Event 1','http://htd.tld/testevent1','Test Event 1','test terms','10% refund','Privacy Policy','http://htd.tld/te1',2),
+	 (6,1,1,'1',1,1,'Test Event 3','http://htd.tld/testevent3','Test Event 3','test terms','10% refund','Privacy Policy','http://htd.tld/te3',2),
+	 (6,1,1,'1',1,1,'Test Event 2','http://htd.tld/testevent2','Test Event 2','test terms','10% refund','Privacy Policy','http://htd.tld/te2',2),
+	 (11,1,1,'1',1,1,'NN Test Event 1','http://htd.tld/nntestevent1','NN Test event 1','test terms','10% refund','Privacy Policy','http://htd.tld/nnte1',1),
+	 (11,1,1,'1',1,1,'NN Test Event 2','http://htd.tld/nntestevent2','NN Test event 2','test terms','10% refund','Privacy Policy','http://htd.tld/nnte2',1),
+	 (11,1,1,'1',1,1,'NN Test Event 3','http://htd.tld/nntestevent3','NN Test Event 1','test terms','10% refund','Privacy Policy','http://htd.tld/nnte3',1),
+	 (6,1,1,'1',1,1,'Test Event 4','http://htd.tld/testevent4','Test Event 4','test terms','10% refund','Privacy Policy','http://htd.tld/te4',2);
